d059c2078eeb094f5ef843b071a38562b32c7aa1
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 23:07:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,7 @@ def adding_matz
 		 programmer_hash[:yukihiro_matsumoto] = {}
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
 end
 
 def changing_alan
@@ -105,6 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
+		 programmer_hash
 end
 
 def adding_to_dennis
@@ -124,4 +126,5 @@ def adding_to_dennis
         }
      }
 		 programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+		 programmer_hash
 end
